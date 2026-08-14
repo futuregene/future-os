fd6044cb2799b84b5cc7450e53e78b2764c6f50a
tallcode <angelxutao@gmail.com>
2026-08-14 18:07:31 +0800

    refactor(markdown): share parser across desktop and mobile

---
+++ markdown/src/index.ts
@@ -0,0 +1,13 @@
+export { basename, localFilePath } from "./localPath";
+export { parseFutureMarkdown } from "./parseFutureMarkdown";
+export { referenceKey } from "./types";
+export type {
+  FutureMarkdownDocument,
+  FutureReference,
+  FutureReferenceType,
+  FutureReferenceView,
+  InlineNode,
+  ListItemNode,
+  MarkdownNode,
+  TableNode,
+} from "./types";
